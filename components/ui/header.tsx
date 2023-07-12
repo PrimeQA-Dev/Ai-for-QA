a0c5bc42e9b96431d99b4b4ec09ec1f1efee288e
Sachin <sachin@primeqasolutions.com>
2023-07-12 15:47:29 +0530

    Adding folders

---
+++ components/ui/header.tsx
@@ -0,0 +1,43 @@
+import Link from 'next/link'
+import MobileMenu from './mobile-menu'
+import Image from 'next/image'
+
+import Logo from '@/public/images/Prime_Black-bg.jpg'
+
+export default function Header() {
+  return (
+    <header className="absolute w-full z-30">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="flex items-center justify-between h-20">
+          {/* Site branding */}
+            {/* Logo */}
+            <a href="/">
+            <Image src={Logo} width={540} height={405} alt="logo" className="w-[150px]" />
+            </a>
+          {/* Desktop navigation */}
+          <nav className="hidden md:flex md:grow">
+            {/* Desktop sign in links */}
+            <ul className="flex grow justify-end flex-wrap items-center">
+              <li>
+                <Link
+                  href="/signin"
+                  className="font-medium text-blue-600 hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out"
+                >
+                  Sign in
+                </Link>
+              </li>
+              <li>
+                <Link href="/signup" className="btn-sm text-white bg-blue-600 hover:bg-blue-700 ml-3">
+                  Sign up
+                </Link>
+              </li>
+            </ul>
+          </nav>
+
+          <MobileMenu />
+
+        </div>
+      </div>
+    </header>
+  )
+}
